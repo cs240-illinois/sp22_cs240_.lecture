aec5d06b53d868d2abb9ead2f6e452df640c61a0
Wade Fagen-Ulmschneider <waf@illinois.edu>
2022-02-07 14:18:52 -0600

    lec7

---
+++ 07/fifteen-threads.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+ 
+const int num_threads = 15;
+ 
+void *thread_start(void *ptr) {
+  int id = *((int *)ptr);
+  printf("Thread %d running...\n", id);
+  return NULL;
+}
+ 
+int main(int argc, char *argv[]) {
+  // Create threads:
+  int i;
+  pthread_t tid[num_threads];
+  for (i = 0; i < num_threads; i++) {
+    pthread_create(&tid[i], NULL, thread_start, (void *)&i);
+  }
+ 
+  printf("Done!\n");
+  return 0;
+}
