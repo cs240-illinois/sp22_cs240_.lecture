8e039526fa5e377e756735f637b161c816c7b77f
Wade Fagen-Ulmschneider <waf@illinois.edu>
2022-03-29 22:30:28 -0500

    18 update

--- 18/s3-put.py
+++ 18/s3-put.py
@@ -1,7 +1,4 @@
-
 import boto3
-import json
-
 s3 = boto3.client('s3', endpoint_url='http://sp22-cs240-001.cs.illinois.edu:9000', aws_access_key_id='minioadmin', aws_secret_access_key='minioadmin')
 
 # Create bucket if it does not exit:
@@ -12,6 +9,10 @@ except:
   # bucket already exists
   print("Bucket `cs240` already exists!")
 
-# Add file:
+import json
+
+# Add an object as a string:
 s3.put_object(Bucket="cs240", Key="session_data", Body=json.dumps({"hello": "world"}))
+
+# Upload a file:
 s3.upload_file("cs240.png", Bucket="cs240", Key="profile-picture.png")
